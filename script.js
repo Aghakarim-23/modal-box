75804aa3a6ccff09740bcbfc2398d3a5e317bae9
Aghakarim-dev <hemidzade1010@gmail.com>
2024-01-31 09:40:03 +0400

    test2

--- script.js
+++ script.js
@@ -4,11 +4,18 @@ const btnModal = document.getElementById("btnModal")
 btnModal.addEventListener("click", ()=> {
     btnModal.classList.add("hidden")
     menu.classList.remove("hidden")
+    container.classList.remove('hidden')
 })
 const ikon = document.getElementById('ikon')
 const menu = document.getElementById('menu')
+const container = document.getElementById('container')
+
 
 ikon.addEventListener("click", ()=>{
 btnModal.classList.remove("hidden")
 menu.classList.add('hidden')
+container.classList.add('hidden')
+
+
 })
+
